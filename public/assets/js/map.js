c532b2b1f13c70652a5ee76c2f77abf3de3792c0
ilhmstxr <ilhambintanghuh30332@gmail.com>
2024-01-10 17:48:04 +0700

    maps

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1,12 +1,68 @@
+const getCurrentPosition = () => {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                latitude = position.coords.latitude;
+                longitude = position.coords.longitude;
+                resolve();
+            },
+            (error) => {
+                reject(error);
+            },
+            {
+                enableHighAccuracy: true,
+                // timeout: 5000,
+            }
+        );
+    });
+};
+
 let map;
 
 async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
+    const currentPosition = await getCurrentPosition();
 
-    map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+    // console.log(latitude);
+    // console.log(longitude);
+
+    pos = {
+        lat: latitude,
+        lng: longitude,
+    };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: { lat: latitude, lng: longitude },
     });
+
+    new google.maps.Marker({
+        position: pos,
+        map,
+    });
+
+    const geocoder = new google.maps.Geocoder();
+    const infoWindow = new google.maps.InfoWindow();
+
+    geocoder
+        .geocode({
+            location: pos,
+        })
+        .then((response) => {
+            if (response.results[0]) {
+                map.setZoom(15);
+
+                const marker = new google.maps.Marker({
+                    position: pos,
+                    map: map,
+                });
+
+                infoWindow.setContent(response.results[0].formatted_address);
+                infoWindow.open(map, marker);
+            } else {
+                infoWindow.setContent(pos);
+                infoWindow.open(map, marker);
+            }
+        });
 }
 
-initMap();
+window.initMap = initMap;
--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -7,12 +7,14 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('assets/css/map.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/map.css') }}">
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+
 </head>
 
 <body>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    {{-- <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Test google maps</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
@@ -44,52 +46,54 @@
                 </ul>
             </div>
         </div>
-    </nav>
+    </nav> --}}
 
     <div id="map"></div>
 
+
+    {{-- <script>
+        (g => {
+            var h, a, k, p = "The Google Maps JavaScript API",
+                c = "google",
+                l = "importLibrary",
+                q = "__ib__",
+                m = document,
+                b = window;
+            b = b[c] || (b[c] = {});
+            var d = b.maps || (b.maps = {}),
+                r = new Set,
+                e = new URLSearchParams,
+                u = () => h || (h = new Promise(async (f, n) => {
+                    await (a = m.createElement("script"));
+                    e.set("libraries", [...r] + "");
+                    for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+                    e.set("callback", c + ".maps." + q);
+                    a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+                    d[q] = f;
+                    a.onerror = () => h = n(Error(p + " could not load."));
+                    a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+                    m.head.append(a)
+                }));
+            d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() =>
+                d[l](f, ...n))
+        })
+        ({
+            key: "AIzaSyB7GbCxzGo3Db3nQrvUL8B0o9L2GcHZpBI",
+            v: "weekly"
+        });
+    </script> --}}
+    <script src="{{ asset('assets/js/map.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/gps.js') }}"></script> --}}
+
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7GbCxzGo3Db3nQrvUL8B0o9L2GcHZpBI&callback=initMap&v=weekly"
+        defer></script>
 </body>
-<script src="{{ asset('assets/js/map.js') }}"></script>
 
-<script>
-    (g => {
-        var h, a, k, p = "The Google Maps JavaScript API",
-            c = "google",
-            l = "importLibrary",
-            q = "__ib__",
-            m = document,
-            b = window;
-        b = b[c] || (b[c] = {});
-        var d = b.maps || (b.maps = {}),
-            r = new Set,
-            e = new URLSearchParams,
-            u = () => h || (h = new Promise(async (f, n) => {
-                await (a = m.createElement("script"));
-                e.set("libraries", [...r] + "");
-                for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
-                e.set("callback", c + ".maps." + q);
-                a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
-                d[q] = f;
-                a.onerror = () => h = n(Error(p + " could not load."));
-                a.nonce = m.querySelector("script[nonce]")?.nonce || "";
-                m.head.append(a)
-            }));
-        d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() =>
-            d[l](f, ...n))
-    })
-    ({
-        key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg",
-        v: "weekly"
-    });
-</script>
-<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 
-<script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7GbCxzGo3Db3nQrvUL8B0o9L2GcHZpBI&callback=console.debug&libraries=maps,marker&v=beta">
-    < script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity = "sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin = "anonymous" >
-</script>
+
+
+
 
 
 </html>
