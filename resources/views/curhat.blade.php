2c57807f4b1d4f355beec5b772090bcdd6d5237e
Sevylo <putrahikmah10@gmail.com>
2024-01-11 23:04:22 +0700

    change: footer layouts, footer views

--- resources/views/curhat.blade.php
+++ resources/views/curhat.blade.php
@@ -27,61 +27,7 @@
 
 </div>
 
-<div class="bgfooter-text pb-2 pt-5 ps-5 ">
-    <div class="row">
-        <div class="col-6">
-            <a href="#" class="no-underline">
-            <h6 class="hpoppins" style="color: #FFFFFF;">SHEA</h6>
-            </a>
-            <p class="pSora text-light pe-5">Jl. Rungkut Madya No. 1, Gn. Anyar, Kec. Gn. Anyar, Surabaya, Jawa Timur 60294</p>
-        </div>
-
-        <div class="col-6 footer-grid pe-5">
-            <div class="link-terkait">
-                <h6 class="hpoppins" style="color: #FFFFFF;">Link Terkait</h6>
-                <ul class="list-group list-group-flush bg-transparent text-start">
-                <li class="list-group-item">
-                <a class="pSora no-underline" href="/menu" style="color: #FFFFFF;">Beranda</a>
-                </li>
-            
-                <li class="list-group-item">
-                <a class="pSora no-underline" href="/curhat" style="color: #FFFFFF;">Curhat</a>
-                </li>
-
-                <li class="list-group-item">
-                <a class="pSora no-underline" href="#" style="color: #FFFFFF;">Forum</a>
-                </li>
-
-                <li class="list-group-item">
-                <a class="pSora no-underline" href="#" style="color: #FFFFFF;">Top Up</a>
-                </li>
-                </ul>
-            </div>
-
-            <div class="social-media col-8 text-center">
-                <h6 class="hpoppins text-light">Social Media</h6>
-                <a href="#">
-                    <img src="{{('/img/logofb.png')}}" alt="ig" width="50px" class="p-2 pb-3 pt-4">
-                </a>
-                <br>
-                <a>
-                <img src="{{('/img/logoig.png')}}" alt="ig" width="50px" class="p-2" onclick="openLinkInNewTab()"> 
-                <script>
-                function openLinkInNewTab() {
-                var linkToOpen = 'https://www.instagram.com/azztqy_';
-                window.open(linkToOpen, '_blank');
-                }
-                </script>
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="line pt-2"></div>
-
-    <div class="container pSora text-center text-light" >
-        <p>Copyright © SHEA 2024</p>
-    </div>
+@include('layouts.footer')
 
 
 @endsection
