14872d0b0f9d3b10b04e69350cfbb38070c5a2f8
Sevylo <putrahikmah10@gmail.com>
2024-01-12 17:14:14 +0700

    change: fix login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header bg-primary text-light">{{ __('') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,10 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Anda Berhasil Login!') }}
+
+                    <a href="/menu" class="wbutton-outline hpoppins" style="font-weight: bold;" role="button">Kembali Ke Menu</a>
+
                 </div>
             </div>
         </div>
