7a3f34ef1f807134aea4b376a332d12cdc3d494e
ilhmstxr <ilhambintanghuh30332@gmail.com>
2024-01-18 21:29:52 +0700

    change: countdown done

--- resources/views/chatify.blade.php
+++ resources/views/chatify.blade.php
@@ -2,20 +2,18 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @section('title','SHEA')
-    <link rel="icon" href="{{('/img/logoSHEA.png')}}">
+    @section('title', 'SHEA')
+    <link rel="icon" href="{{ '/img/logoSHEA.png' }}">
 </head>
+
 <body>
     @section('content')
-        
 
-
-
-
-
-    @endsection    
+    @endsection
 </body>
+
 </html>
